66126fdfaf455324a5370cbc024d9e4f70a247ff
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-10-04 00:46:19 +0500

    marge frontend and backend

--- next-frontend/frontend/app/page.tsx
+++ next-frontend/frontend/app/page.tsx
@@ -1,6 +1,7 @@
-"use client";
+
 import React from 'react'
 import { client } from './utils/configSanity';
+import PortableText from "react-portable-text"
 
 interface IBlog {
   _id: string;
@@ -11,24 +12,36 @@ interface IBlog {
 
 async function getBlog() {
   const query = '*[_type == "blog"]';
-  const blog = await client.fetch(query);
-  return blog as IBlog[];
+  const blogs = await client.fetch(query);
+  return blogs as IBlog[];
 }
 
 export default async function Home () {
-   const blog = (await getBlog()) as IBlog[];
-  console.log(blog);
+   const blogs = (await getBlog()) as IBlog[];
+  console.log(blogs[2]);
   
   return (
    <div className='flex '>
     <div className='flex gap-10'>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
+      <p>{blogs[2].title}</p>
+      <p>{blogs[2].description}</p>
+      {/* <p>{blogs[2]._id}</p> */}
+ 
+      <PortableText
+      // Pass in block content straight from Sanity.io
+      content={blogs[2].content}
+      // Transform content
+      projectId = "o8l7uk7j"
+      dataset = "production"
+      // Optionally override marks, decorators, blocks, etc. in a flat
+      // structure without doing any gymnastics
+      serializers={{
+        h1: (props: any) => <h1 style={{ color: "red" }} {...props} />,
+        li: ({ children }:any) => <li className="special-list-item">{children}</li>,
+       
+      }}
+      
+    />
     </div>
    </div>
   )
--- next-frontend/frontend/app/utils/configSanity.ts
+++ next-frontend/frontend/app/utils/configSanity.ts
@@ -4,5 +4,5 @@ export const client = createClient({
     projectId: "o8l7uk7j",
     dataset: "production",
     apiVersion: "2022-03-10",
-    useCdn: true,
+    useCdn: false,
 })
